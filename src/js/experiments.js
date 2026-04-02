0e500b0b83c17a765c86ba9e5f18363565447416
ivsanpedro <ivsanpedro2@gmail.com>
2026-04-02 16:18:36 -0400

    Complete 5 experiments

--- src/js/experiments.js
+++ src/js/experiments.js
@@ -8,14 +8,14 @@ const heading = document.querySelector('h1');
 heading.textContent = 'Welcome to My Awesome Site 🏋🏻‍♀️';
 
 // Experiment 2: Counting the number of workouts in the data and displaying it in the console
-const allOptions = document.querySelectorAll('option');
-console.log(`This page has ${allOptions.length} total options.`);
+// const allOptions = document.querySelectorAll('option');
+// console.log(`This page has ${allOptions.length} total options.`);
 
 // Counting the number of options in the equipment dropdown and displaying it in the console
-const typeDropdown = document.querySelector('#equipment');
-console.log(
-  `The equipment dropdown has ${typeDropdown.children.length} options.`
-);
+// const typeDropdown = document.querySelector('#equipment');
+// console.log(
+//   `The equipment dropdown has ${typeDropdown.children.length} options.`
+// );
 
 // Experiment 3: Adding a footer into the HTML
 const footer = document.createElement('footer');
@@ -28,3 +28,17 @@ document.body.appendChild(footer);
 // Adds a light pink border to the form section to highlight it as part of an experiment.
 const formSection = document.querySelector('.left-col');
 formSection.classList.add('highlight-experiment');
+
+//Experiment 5: Make text blink
+const subtitle = document.querySelector('.subtitle');
+
+let isVisible = true;
+setInterval(() => {
+  if (isVisible) {
+    subtitle.classList.add('hidden');
+  } else {
+    subtitle.classList.remove('hidden');
+  }
+  isVisible = !isVisible;
+}, 1500);
+// Runs every 1.5 seconds to toggle the visibility of the subtitle, creating a blinking effect.
